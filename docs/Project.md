56d6d0cadca3429881d734899ceffb9135e1f041
wabliplatform <93119230+wabliplatform@users.noreply.github.com>
2022-03-24 14:56:39 +0200

    My commit message

---
+++ docs/Project.md
@@ -0,0 +1,19 @@
+# TempApi.Project
+
+## Properties
+
+Name | Type | Description | Notes
+------------ | ------------- | ------------- | -------------
+**id** | **String** |  | [optional] 
+**pImage** | [**ProjectPImage**](ProjectPImage.md) |  | 
+**pTitle** | **String** |  | 
+**pStart** | **String** |  | 
+**pWebsite** | **String** |  | 
+**pEnd** | **String** |  | 
+**pDuration** | **String** |  | 
+**pGA** | **String** |  | 
+**pAbstract** | **String** |  | 
+**pDescription** | **String** |  | 
+**pWorkpackage** | [**[Workpackage]**](Workpackage.md) |  | 
+
+
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+	'homePage' : './javascript/homePage.js',
+	'createProject' : './javascript/createProject.js',
+	'updateProject' : './javascript/updateProject.js',
+	'viewProject' : './javascript/viewProject.js',
+	'createTask' : './javascript/createTask.js',
+	'updateTask' : './javascript/updateTask.js',
+	'viewTask' : './javascript/viewTask.js',
+	'viewTasks' : './javascript/viewTasks.js',
+	'createWorkpackage' : './javascript/createWorkpackage.js',
+	'updateWorkpackage' : './javascript/updateWorkpackage.js',
+	'createEmployee' : './javascript/createEmployee.js',
+	'viewEmployees' : './javascript/viewEmployees.js',
+	'viewWorkpackages' : './javascript/viewWorkpackages.js',
+},
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+    
+  },
+};
